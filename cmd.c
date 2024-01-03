SgtBilko76/Xonotic-QuestVR

commit b65e4e0c16f92e934ddb5adb324bfa0c4a38b9bd

con: allow alias loops involving wait to be broken, eg with unalias or quit
Signed-off-by: bones_was_here <bones_was_here@xonotic.au>

cmd.c
@@ -425,8 +425,10 @@ static void Cbuf_Frame_Input(void)
 {
 	char *line;
 
+	// bones_was_here: prepending allows a loop such as `alias foo "bar; wait; foo"; foo`
+	// to be broken with an alias or unalias command
 	while ((line = Sys_ConsoleInput()))
-			Cbuf_AddText(cmd_local, line);
+		Cbuf_InsertText(cmd_local, line);
 }
 
 void Cbuf_Frame(cmd_buf_t *cbuf)

keys.c
@@ -1098,8 +1098,7 @@ static int Key_Convert_NumPadKey(int key)
 	return key;
 }
 
-static void
-Key_Console(cmd_state_t *cmd, int key, int unicode)
+static void Key_Console(cmd_state_t *cmd, int key, int unicode)
 {
 	int linepos;
 
@@ -1121,8 +1120,9 @@ Key_Console(cmd_state_t *cmd, int key, int unicode)
 
 	if ((key == K_ENTER || key == K_KP_ENTER) && KM_NONE)
 	{
-		Cbuf_AddText (cmd, key_line+1);	// skip the ]
-		Cbuf_AddText (cmd, "\n");
+		// bones_was_here: prepending allows a loop such as `alias foo "bar; wait; foo"; foo`
+		// to be broken with an alias or unalias command
+		Cbuf_InsertText(cmd, key_line+1); // skip the ]
 		Key_History_Push();
 		key_linepos = Key_ClearEditLine(true);
 		// force an update, because the command may take some time

commit f9bf3e3ea884eef6a9f36e22f26b33f26f3e48ec

cmd: fix stdin command ordering and related stuff
Fixes bug introduced in b65e4e0c16f92e934ddb5adb324bfa0c4a38b9bd where
multiline stdin execution order was reversed.

Retrieves FD of stdin stream rather than assuming it will always be 0.

Signed-off-by: bones_was_here <bones_was_here@xonotic.au>

cmd.c
@@ -425,10 +425,20 @@ static void Cbuf_Frame_Input(void)
 {
 	char *line;
 
-	// bones_was_here: prepending allows a loop such as `alias foo "bar; wait; foo"; foo`
-	// to be broken with an alias or unalias command
-	while ((line = Sys_ConsoleInput()))
+	if ((line = Sys_ConsoleInput()))
+	{
+		// bones_was_here: prepending allows a loop such as `alias foo "bar; wait; foo"; foo`
+		// to be broken with an alias or unalias command
 		Cbuf_InsertText(cmd_local, line);
+		/* appending subsequent lines allows this test to pass (if pasted with proper \n):
+		 * wait; echo a
+		 * wait
+		 * echo b
+		 * echo c
+		 */
+		while ((line = Sys_ConsoleInput()))
+			Cbuf_AddText(cmd_local, line);
+	}
 }
 
 void Cbuf_Frame(cmd_buf_t *cbuf)

sys_shared.c
@@ -618,8 +618,8 @@ void Sys_Printf(const char *fmt, ...)
 char *Sys_ConsoleInput(void)
 {
 	static char text[MAX_INPUTLINE];
-	static unsigned int len = 0;
 #ifdef WIN32
+	static unsigned int len = 0;
 	int c;
 
 	// read a line out
@@ -653,24 +653,12 @@ char *Sys_ConsoleInput(void)
 	}
 #else
 	fd_set fdset;
-	struct timeval timeout;
+	struct timeval timeout = { .tv_sec = 0, .tv_usec = 0 };
+
 	FD_ZERO(&fdset);
-	FD_SET(0, &fdset); // stdin
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 0;
-	if (select (1, &fdset, NULL, NULL, &timeout) != -1 && FD_ISSET(0, &fdset))
-	{
-		len = read (0, text, sizeof(text) - 1);
-		if (len >= 1)
-		{
-			// rip off the \n and terminate
-			// div0: WHY? console code can deal with \n just fine
-			// this caused problems with pasting stuff into a terminal window
-			// so, not ripping off the \n, but STILL keeping a NUL terminator
-			text[len] = 0;
-			return text;
-		}
-	}
+	FD_SET(fileno(stdin), &fdset);
+	if (select(1, &fdset, NULL, NULL, &timeout) != -1 && FD_ISSET(fileno(stdin), &fdset))
+		return fgets(text, sizeof(text), stdin);
 #endif
 	return NULL;
 }
@@ -922,7 +910,7 @@ int main (int argc, char **argv)
 	Sys_ProvideSelfFD(); // may call Con_Printf() so must be after sys.outfd is set
 
 #ifndef WIN32
-	fcntl(0, F_SETFL, fcntl (0, F_GETFL, 0) | O_NONBLOCK);
+	fcntl(fileno(stdin), F_SETFL, fcntl (fileno(stdin), F_GETFL, 0) | O_NONBLOCK);
 #endif
 
 #ifdef __ANDROID__