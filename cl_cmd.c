SgtBilko76/Xonotic-QuestVR

commit 7b3021e60dd1fce29a4de76e3187b402bf5cf390

Make `name` be a command or virtual cvar on a game-specific basis
This allows matching the client console behaviour of div0-stable in
games that want it.

Closes https://gitlab.com/xonotic/darkplaces/-/issues/337

Signed-off-by: bones_was_here <bones_was_here@xonotic.au>

cl_cmd.c
@@ -28,7 +28,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "cl_collision.h"
 
-cvar_t cl_name = {CF_CLIENT | CF_ARCHIVE | CF_USERINFO, "name", "player", "player name"};
+cvar_t cl_name = {CF_CLIENT | CF_ARCHIVE | CF_USERINFO, "_cl_name", "player", "player name"};
 cvar_t cl_rate = {CF_CLIENT | CF_ARCHIVE | CF_USERINFO, "rate", "20000", "connection speed"};
 cvar_t cl_rate_burstsize = {CF_CLIENT | CF_ARCHIVE | CF_USERINFO, "rate_burstsize", "1024", "rate control burst size"};
 cvar_t cl_topcolor = {CF_CLIENT | CF_ARCHIVE | CF_USERINFO, "topcolor", "0", "color of your shirt"};
@@ -214,6 +214,32 @@ static void CL_SendCvar_f(cmd_state_t *cmd)
 			CL_ForwardToServer(va(vabuf, sizeof(vabuf), "sentcvar %s \"%s\"", c->name, c->string));
 		return;
 	}
+}
+
+/*
+==================
+CL_Name_f
+
+The logic from div0-stable's Host_Name_f() is now in SV_Name_f().
+==================
+*/
+static void CL_Name_f(cmd_state_t *cmd)
+{
+	char *newNameSource;
+
+	if (Cmd_Argc(cmd) == 1)
+	{
+		Con_Printf("name: \"%s^7\"\n", cl_name.string);
+		return;
+	}
+
+	// in the single-arg case any enclosing quotes shall be stripped
+	newNameSource = (char *)(Cmd_Argc(cmd) == 2 ? Cmd_Argv(cmd, 1) : Cmd_Args(cmd));
+
+	if (strlen(newNameSource) >= MAX_SCOREBOARDNAME) // may as well truncate before networking
+		newNameSource[MAX_SCOREBOARDNAME - 1] = '\0'; // this is fine (cbuf stores length)
+
+	Cvar_SetQuick(&cl_name, newNameSource);
 }
 
 /*
@@ -715,8 +741,16 @@ void CL_InitCommands(void)
 {
 	dpsnprintf(cls.userinfo, sizeof(cls.userinfo), "\\name\\player\\team\\none\\topcolor\\0\\bottomcolor\\0\\rate\\10000\\msg\\1\\noaim\\1\\*ver\\dp");
 
+	/* In Quake `name` is a command that concatenates its arguments (quotes unnecessary)
+	 * which is expected in most DP-based games.
+	 * In QuakeWorld it's a cvar which requires quotes if spaces are used.
+	 */
 	Cvar_RegisterVariable(&cl_name);
-	Cvar_RegisterVirtual(&cl_name, "_cl_name");
+	if ((0)) // TODO: if (gamemode == GAME_QUAKEWORLD)
+		Cvar_RegisterVirtual(&cl_name, "name");
+	else
+		Cmd_AddCommand(CF_CLIENT, "name", CL_Name_f, "change your player name");
+
 	Cvar_RegisterVariable(&cl_rate);
 	Cvar_RegisterVirtual(&cl_rate, "_cl_rate");
 	Cvar_RegisterVariable(&cl_rate_burstsize);

commit 1c1ea8484e51633e370a87a50fbd4ecbcf73205a

Fix changing of player name during a game
Name changes applied when connecting or when the map changed, but not
during the game.
I broke it in 7b3021e60dd1fce29a4de76e3187b402bf5cf390 by not
noticing the user-visible name of the cvar is used to determine which
command (if any) is sent.

Signed-off-by: bones_was_here <bones_was_here@xonotic.au>

cl_cmd.c
@@ -28,6 +28,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "cl_collision.h"
 
+/// User-visible names of these CF_USERINFO cvars must be matched in CL_SetInfo()!
 cvar_t cl_name = {CF_CLIENT | CF_ARCHIVE | CF_USERINFO, "_cl_name", "player", "player name"};
 cvar_t cl_rate = {CF_CLIENT | CF_ARCHIVE | CF_USERINFO, "rate", "20000", "connection speed"};
 cvar_t cl_rate_burstsize = {CF_CLIENT | CF_ARCHIVE | CF_USERINFO, "rate_burstsize", "1024", "rate control burst size"};
@@ -37,6 +38,7 @@ cvar_t cl_team = {CF_CLIENT | CF_USERINFO | CF_ARCHIVE, "team", "none", "QW team
 cvar_t cl_skin = {CF_CLIENT | CF_USERINFO | CF_ARCHIVE, "skin", "", "QW player skin name (example: base)"};
 cvar_t cl_noaim = {CF_CLIENT | CF_USERINFO | CF_ARCHIVE, "noaim", "1", "QW option to disable vertical autoaim"};
 cvar_t cl_pmodel = {CF_CLIENT | CF_USERINFO | CF_ARCHIVE, "pmodel", "0", "current player model number in nehahra"};
+
 cvar_t r_fixtrans_auto = {CF_CLIENT, "r_fixtrans_auto", "0", "automatically fixtrans textures (when set to 2, it also saves the fixed versions to a fixtrans directory)"};
 
 extern cvar_t rcon_secure;

cl_main.c
@@ -257,7 +257,7 @@ void CL_SetInfo(const char *key, const char *value, qbool send, qbool allowstark
 			MSG_WriteByte(&cls.netcon->message, qw_clc_stringcmd);
 			MSG_WriteString(&cls.netcon->message, va(vabuf, sizeof(vabuf), "setinfo \"%s\" \"%s\"", key, value));
 		}
-		else if (!strcasecmp(key, "name"))
+		else if (!strcasecmp(key, "_cl_name") || !strcasecmp(key, "name"))
 		{
 			MSG_WriteByte(&cls.netcon->message, clc_stringcmd);
 			MSG_WriteString(&cls.netcon->message, va(vabuf, sizeof(vabuf), "name \"%s\"", value));