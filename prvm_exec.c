SgtBilko76/Xonotic-QuestVR

commit c53549fb0ac188ba5d1c2fc05082f466dd3b32dc

host: Move out some client and server code
git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@13015 d7cf8633-e32d-0410-b094-e92efae38249

prvm_exec.c
@@ -715,6 +715,9 @@ extern cvar_t prvm_errordump;
 void PRVM_Crash(prvm_prog_t *prog)
 {
 	char vabuf[1024];
+
+	cl.csqc_loaded = false;
+
 	if (prog == NULL)
 		return;
 	if (!prog->loaded)

cd_shared.c
@@ -122,7 +122,7 @@ static void CDAudio_Play_byName (const char *trackname, qbool looping, qbool try
 	sfx_t* sfx;
 	char filename[MAX_QPATH];
 
-	Host_StartVideo();
+	CL_StartVideo();
 
 	if (!enabled)
 		return;
@@ -275,7 +275,7 @@ static void CD_f(cmd_state_t *cmd)
 	command = Cmd_Argv(cmd, 1);
 
 	if (strcasecmp(command, "remap") != 0)
-		Host_StartVideo();
+		CL_StartVideo();
 
 	if (strcasecmp(command, "on") == 0)
 	{

cl_main.c
@@ -2728,6 +2728,21 @@ void CL_UpdateEntityShading(void)
 		CL_UpdateEntityShading_Entity(r_refdef.scene.entities[i]);
 }
 
+qbool vid_opened = false;
+void CL_StartVideo(void)
+{
+	if (!vid_opened && cls.state != ca_dedicated)
+	{
+		vid_opened = true;
+#ifdef WIN32
+		// make sure we open sockets before opening video because the Windows Firewall "unblock?" dialog can screw up the graphics context on some graphics drivers
+		NetConn_UpdateSockets();
+#endif
+		VID_Start();
+		CDAudio_Startup();
+	}
+}
+
 extern cvar_t host_framerate;
 extern cvar_t host_speeds;
 

cl_video.c
@@ -577,7 +577,7 @@ void CL_DrawVideo(void)
 
 void CL_VideoStart(char *filename, const char *subtitlesfile)
 {
-	Host_StartVideo();
+	CL_StartVideo();
 
 	if( cl_videos->state != CLVIDEO_UNUSED )
 		CL_CloseVideo( cl_videos );
@@ -615,7 +615,7 @@ static void CL_PlayVideo_f(cmd_state_t *cmd)
 	char name[MAX_QPATH], subtitlesfile[MAX_QPATH];
 	const char *extension;
 
-	Host_StartVideo();
+	CL_StartVideo();
 
 	if (Sys_CheckParm("-benchmark"))
 		return;

client.h
@@ -1214,6 +1214,8 @@ double CL_Frame(double time);
 void CL_Shutdown (void);
 void CL_Init (void);
 
+void CL_StartVideo(void);
+
 void CL_EstablishConnection(const char *host, int firstarg);
 
 void CL_Disconnect (void);

host.c
@@ -131,7 +131,6 @@ void Host_Error (const char *error, ...)
 	PRVM_Crash(MVM_prog);
 #endif
 
-	cl.csqc_loaded = false;
 	Cvar_SetValueQuick(&csqc_progcrc, -1);
 	Cvar_SetValueQuick(&csqc_progsize, -1);
 
@@ -148,56 +147,6 @@ void Host_Error (const char *error, ...)
 	hosterror = false;
 
 	Host_AbortCurrentFrame();
-}
-
-static void Host_ServerOptions (void)
-{
-	int i;
-
-	// general default
-	svs.maxclients = 8;
-
-// COMMANDLINEOPTION: Server: -dedicated [playerlimit] starts a dedicated server (with a command console), default playerlimit is 8
-// COMMANDLINEOPTION: Server: -listen [playerlimit] starts a multiplayer server with graphical client, like singleplayer but other players can connect, default playerlimit is 8
-	// if no client is in the executable or -dedicated is specified on
-	// commandline, start a dedicated server
-	i = Sys_CheckParm ("-dedicated");
-	if (i || !cl_available)
-	{
-		cls.state = ca_dedicated;
-		// check for -dedicated specifying how many players
-		if (i && i + 1 < sys.argc && atoi (sys.argv[i+1]) >= 1)
-			svs.maxclients = atoi (sys.argv[i+1]);
-		if (Sys_CheckParm ("-listen"))
-			Con_Printf ("Only one of -dedicated or -listen can be specified\n");
-		// default sv_public on for dedicated servers (often hosted by serious administrators), off for listen servers (often hosted by clueless users)
-		Cvar_SetValue(&cvars_all, "sv_public", 1);
-	}
-	else if (cl_available)
-	{
-		// client exists and not dedicated, check if -listen is specified
-		cls.state = ca_disconnected;
-		i = Sys_CheckParm ("-listen");
-		if (i)
-		{
-			// default players unless specified
-			if (i + 1 < sys.argc && atoi (sys.argv[i+1]) >= 1)
-				svs.maxclients = atoi (sys.argv[i+1]);
-		}
-		else
-		{
-			// default players in some games, singleplayer in most
-			if (gamemode != GAME_GOODVSBAD2 && !IS_NEXUIZ_DERIVED(gamemode) && gamemode != GAME_BATTLEMECH)
-				svs.maxclients = 1;
-		}
-	}
-
-	svs.maxclients = svs.maxclients_next = bound(1, svs.maxclients, MAX_SCOREBOARD);
-
-	svs.clients = (client_t *)Mem_Alloc(sv_mempool, sizeof(client_t) * svs.maxclients);
-
-	if (svs.maxclients > 1 && !deathmatch.integer && !coop.integer)
-		Cvar_SetValueQuick(&deathmatch, 1);
 }
 
 /*
@@ -522,21 +471,6 @@ void Host_Main(void)
 
 //============================================================================
 
-qbool vid_opened = false;
-void Host_StartVideo(void)
-{
-	if (!vid_opened && cls.state != ca_dedicated)
-	{
-		vid_opened = true;
-#ifdef WIN32
-		// make sure we open sockets before opening video because the Windows Firewall "unblock?" dialog can screw up the graphics context on some graphics drivers
-		NetConn_UpdateSockets();
-#endif
-		VID_Start();
-		CDAudio_Startup();
-	}
-}
-
 char engineversion[128];
 
 qbool sys_nostdout = false;
@@ -713,7 +647,6 @@ static void Host_Init (void)
 	World_Init();
 	SV_Init();
 	Host_InitLocal();
-	Host_ServerOptions();
 
 	Thread_Init();
 	TaskQueue_Init();
@@ -740,7 +673,7 @@ static void Host_Init (void)
 
 	host.state = host_active;
 
-	Host_StartVideo();
+	CL_StartVideo();
 
 	Log_Start();
 

host.h
@@ -42,7 +42,6 @@ void Host_InitCommands(void);
 void Host_Main(void);
 double Host_Frame(double time);
 void Host_Shutdown(void);
-void Host_StartVideo(void);
 void Host_Error(const char *error, ...) DP_FUNC_PRINTF(1) DP_FUNC_NORETURN;
 void Host_LockSession(void);
 void Host_UnlockSession(void);

menu.c
@@ -5470,7 +5470,7 @@ static void Call_MR_ToggleMenu_f(cmd_state_t *cmd)
 {
 	int m;
 	m = ((Cmd_Argc(cmd) < 2) ? -1 : atoi(Cmd_Argv(cmd, 1)));
-	Host_StartVideo();
+	CL_StartVideo();
 	if(MR_ToggleMenu)
 		MR_ToggleMenu(m);
 }

commit 5080148f019fcb87802fcc7096ce79c5287b19f2

PRVM: don't break csqc when menuqc has a problem
Fixes a regression in c53549fb0ac188ba5d1c2fc05082f466dd3b32dc caused by
moving this line such that it executes even for MVM errors. We don't
need it because it's done by Host_Error->CL_DisconnectEx->CL_VM_ShutDown

Signed-off-by: bones_was_here <bones_was_here@xonotic.au>

prvm_exec.c
@@ -720,8 +720,6 @@ void PRVM_Crash(prvm_prog_t *prog)
 	char vabuf[1024];
 	int outfd = sys.outfd;
 
-	cl.csqc_loaded = false;
-
 	if (prog == NULL)
 		return;
 	if (!prog->loaded)