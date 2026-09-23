cd3eecabce84c217b5af9a2723661dba3aa6bc95
XonoticQuest <svensieweke@gmail.com>
2026-09-23 14:52:15 +0200

    Bump version to 0.4.0 for the Beta-0.4 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sgtbilko.xonoticquest"
         minSdk = 26
         targetSdk = 32
-        versionCode = 9
-        versionName = "0.3.1"
+        versionCode = 10
+        versionName = "0.4.0"
 
         externalNativeBuild {
             cmake {
